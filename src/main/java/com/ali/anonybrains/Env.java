a3384e3bb33232b0d5193b139c0bbd7443fbe724
力君 <zhanlijun.zlj@alibaba-inc.com>
2016-07-10 15:11:16 +0800

    s

---
+++ src/main/java/com/ali/anonybrains/Env.java
@@ -0,0 +1,6 @@
+package com.ali.anonybrains;
+
+public class Env {
+    public static final String CORP_ID = "ding6da2053ee844aee8";
+    public static final String CORP_SECRET = "Bg18joX0Bp5u4mvjVF_H7OIMM7O_XrPViUSbI2vOLrbhnU2nXSFYfzGatt5HrIa5";
+}
